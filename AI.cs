24c50ad6014f1b3e632244e154e874e2db58c81f
Janne Koskinen <23258606+koskinen-jam@users.noreply.github.com>
2022-12-05 00:41:30 +0200

    Add some simple AI models

--- AI.cs
+++ AI.cs
@@ -2,7 +2,6 @@ namespace RockPaperScissors
 {
 	namespace AI
 	{
-
 		// AI player that always plays a rock.
 		public class AlwaysRock : AIPlayer
 		{
@@ -13,6 +12,38 @@ namespace RockPaperScissors
 			public override Move GetNextMove()
 			{
 				return Move.Rock();
+			}
+		}
+
+		// AI player that repeats whatever you played last
+		public class RepeatsYourLastMove : AIPlayer
+		{
+			public RepeatsYourLastMove(PlayerInfo info) : base(info) { }
+
+			public override Move GetNextMove()
+			{
+				if (game == null || game.lastRound == null)
+				{
+					return RandomMove();
+				}
+				
+				return game.lastRound.OpponentsMove(this).Clone();
+			}
+		}
+
+		// AI player that picks whatever would beat your previous move
+		public class BeatsYourLastMove : AIPlayer
+		{
+			public BeatsYourLastMove(PlayerInfo info) : base(info) { }
+
+			public override Move GetNextMove()
+			{
+				if (game == null || game.lastRound == null)
+				{
+					return RandomMove();
+				}
+
+				return game.lastRound.OpponentsMove(this).LosesTo();
 			}
 		}
 	}
