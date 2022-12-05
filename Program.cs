9f38c59020d362e2d4036410b265e1306fcc1858
Janne Koskinen <23258606+koskinen-jam@users.noreply.github.com>
2022-12-05 18:01:06 +0200

    Make AIs play each other, reformat output

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@ using RockPaperScissors.AI;
 
 PlayerController control = new PlayerController();
 
-Player me = new HumanPlayer(new PlayerInfo("You"), control);
+/* Player me = new HumanPlayer(new PlayerInfo("You"), control); */
 /* Player ai = new AlwaysRock(new PlayerInfo("AI")); */
-Player ai = new RepeatsYourLastMove(new PlayerInfo("AI"));
+Player me = new RepeatsYourLastMove(new PlayerInfo("AI 1"));
+Player ai = new RepeatsYourLastMove(new PlayerInfo("AI 2"));
 
 bool stop = false;
 while (! stop)
@@ -16,21 +17,28 @@ while (! stop)
 	
 	while (! g.over)
 	{
-		Console.WriteLine($"\t--- Round {g.currentRound} ---");
+		/* Console.WriteLine($"\t--- Round {g.currentRound} ---"); */
 
-		Console.Write($"{g.p1}, choose move: [rps]> ");
+		Console.Write($"  [{g.currentRound, 3}] ");
+
+		if (me is HumanPlayer)
+		{
+			Console.Write($"{g.p1}, choose move: [rps]> ");
+		}
 
 		Move myMove = me.GetNextMove();
 
-		Console.WriteLine($"{myMove}");
+		Console.Write(me is HumanPlayer
+				? $"{$"{myMove}.", -9}"
+				: $"{me.info.name, 6} chose {$"{myMove}.", -9} ");
 		
 		Move aiMove = ai.GetNextMove();
 
-		Console.WriteLine($"{ai.info.name} chose {aiMove}");
+		Console.Write($"{ai.info.name, 6} chose {$"{aiMove}.", -9} ");
 
 		g.Play(myMove, aiMove);
 
-		Console.WriteLine($"{g.lastRound}");
+		Console.WriteLine($"{g.LastRoundReport()}");
 	}
 
 	Console.WriteLine($"\n{g}\n");
